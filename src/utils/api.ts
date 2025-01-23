36871d05746f2ced255d16eed9b5c859fddae6cb
kabir4874 <kabir.cse.bd@gmail.com>
2025-01-23 21:59:03 +0600

    update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,32 @@
 const OPEN_WEATHER_API_KEY = "50645afe7dc713ebcce15941bd98bf95";
-
+export interface OpenWeatherData {
+  name: string;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }[];
+  wind: {
+    deg: number;
+    speed: number;
+  };
+}
 export async function fetchOpenWeatherData(city: string): Promise<any> {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
   );
   if (!res.ok) {
     throw new Error("City not found");
   }
-  const data = await res.json();
+  const data: OpenWeatherData = await res.json();
   return data;
 }
