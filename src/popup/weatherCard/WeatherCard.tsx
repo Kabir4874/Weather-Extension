e1297a1126645fee118a0516fad34f97c8d31412
kabir4874 <kabir.cse.bd@gmail.com>
2025-01-24 16:13:55 +0600

    update

--- src/popup/weatherCard/WeatherCard.tsx
+++ src/popup/weatherCard/WeatherCard.tsx
@@ -1,12 +1,24 @@
-import React, { useEffect } from "react";
-import { fetchOpenWeatherData } from "../../utils/api";
-
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { fetchOpenWeatherData, OpenWeatherData } from "../../utils/api";
 const WeatherCard: React.FC<{ city: string }> = ({ city }) => {
+  const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null);
   useEffect(() => {
     fetchOpenWeatherData(city)
-      .then((data) => console.log(data))
+      .then((data) => setWeatherData(data))
       .catch((err) => console.log(err));
   }, [city]);
-  return <div>{city}</div>;
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h5">{weatherData.name}</Typography>
+        <Typography variant="body1">{weatherData.main.temp}</Typography>
+        <Typography variant="body1">{weatherData.main.feels_like}</Typography>
+      </CardContent>
+    </Card>
+  );
 };
 export default WeatherCard;
