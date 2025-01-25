525738ad264822a7af06c9786b320eaf11bb81ef
kabir4874 <kabir.cse.bd@gmail.com>
2025-01-25 20:35:33 +0600

    update

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Card,
   CardContent,
   Grid,
@@ -7,21 +8,56 @@ import {
   Typography,
 } from "@material-ui/core";
 import "fontsource-roboto";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import {
+  getStoredOptions,
+  LocalStorageOptions,
+  setStoredOptions,
+} from "../utils/storage";
 import "./options.css";
 const App: React.FC<{}> = () => {
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null);
+  useEffect(() => {
+    getStoredOptions().then((options) => setOptions(options));
+  }, []);
+
+  const handleHomeCityChange = (homeCity: string) => {
+    setOptions({ ...options, homeCity });
+  };
+
+  const handleSaveButtonClick = () => {
+    setStoredOptions(options);
+  };
+
+  if (!options) {
+    return null;
+  }
   return (
-    <Box>
+    <Box mx={"10%"} my={"2%"}>
       <Card>
         <CardContent>
-          <Grid container direction="column">
+          <Grid container direction="column" spacing={4}>
             <Grid item>
               <Typography variant="h4">Weather Extension Options</Typography>
             </Grid>
             <Grid item>
               <Typography variant="body1">Home City Name</Typography>
-              <TextField fullWidth placeholder="Enter a home city name" />
+              <TextField
+                fullWidth
+                placeholder="Enter a home city name"
+                value={options.homeCity}
+                onChange={(e) => handleHomeCityChange(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSaveButtonClick}
+              >
+                Save
+              </Button>
             </Grid>
           </Grid>
         </CardContent>
